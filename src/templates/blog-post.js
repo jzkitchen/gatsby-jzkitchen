e8464bc5ca3079f53d0faad8e44f500e06c29697
Jason Lee <eyeshadow.lee@gmail.com>
2020-12-22 10:47:57 +0800

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -125,6 +125,7 @@ export const pageQuery = graphql`
         featuredImage {
           childImageSharp {
             fluid(maxWidth: 1200, maxHeight: 675, quality: 80, srcSetBreakpoints: [320, 400, 480, 560, 640, 720, 800, 960, 1024, 1200]) {
+              ...GatsbyImageSharpFluid
               ...GatsbyImageSharpFluid_withWebp
               ...GatsbyImageSharpFluidLimitPresentationSize
             }
